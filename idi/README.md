2c08d5e3408772d2b0d1db0d59a9d8684709e62e
Francesco Degrassi <francesco.degrassi@optionfactory.net>
2023-03-10 00:01:10 +0100

    :new: check codice fiscale

---
+++ idi/README.md
@@ -0,0 +1,12 @@
+```sql
+DROP EXTENSION IF EXISTS idi;
+CREATE EXTENSION idi;
+```
+
+```sql
+SELECT is_valid_fiscal_code('RSSMRA85T10A562S');
+
+CREATE TABLE users (id INT, cf TEXT CHECK(is_valid_fiscal_code(cf)));
+INSERT INTO users (id, cf) VALUES (1, 'asd');
+```
+
--- idi/src/lib.rs
+++ idi/src/lib.rs
@@ -2,10 +2,31 @@ use pgx::prelude::*;
 
 pg_module_magic!();
 
+#[pg_extern]
+pub fn is_valid_fiscal_code(val: &str) -> bool {
+    codice_fiscale::CodiceFiscale::check(val).is_ok()
+}
+
 #[cfg(any(test, feature = "pg_test"))]
 #[pg_schema]
 mod tests {
-    // TODO
+    use pgx::prelude::*;
+    use crate::is_valid_fiscal_code;
+
+    #[pg_test]
+    fn valid_code_is_valid() {
+        assert!(is_valid_fiscal_code("RSSMRA85T10A562S"));
+    }
+
+    #[pg_test]
+    fn invalid_code_is_invalid() {
+        assert_eq!(false, is_valid_fiscal_code("XXXX"));
+    }
+
+    #[pg_test]
+    fn can_check_is_valid_via_select() {
+        assert_eq!(Some(true), Spi::get_one("SELECT is_valid_fiscal_code('RSSMRA85T10A562S')").unwrap());
+    }
 }
 
 /// This module is required by `cargo pgx test` invocations. 
